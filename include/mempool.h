99f5ca3330d574b64a0d46a56ecc1dc47fcd2078
Mateusz Kiełbasa <sobriodev@gmail.com>
2021-03-13 15:37:04 +0100

    ENH/9 Add draft for mempool files
    Add empty header/source files and unit test executable.

---
+++ include/mempool.h
@@ -0,0 +1,23 @@
+#ifndef MEMPOOL_MEMPOOL_H
+#define MEMPOOL_MEMPOOL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ------------------------------------------------------------ */
+/* ---------------------------- Macros ------------------------ */
+/* ------------------------------------------------------------ */
+
+/** Major version */
+#define MEMPOOL_API_VERSION_MAJOR 0
+/** Minor version */
+#define MEMPOOL_API_VERSION_MINOR   1
+/** Revision version */
+#define MEMPOOL_API_VERSION_REVISION 0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //MEMPOOL_MEMPOOL_H
