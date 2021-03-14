c2089fd2d2e61270618953d7d895b427b8c6cd50
Mateusz Kiełbasa <sobriodev@gmail.com>
2021-03-14 15:02:18 +0100

    ENH/9 Add API functions for mempool module
    Add data types:
    - mempool_status
    - mempool_instance
    - mempool_debug_info
    
    Add API functions:
    - mempool_init()
    - mempool_calc_hdr_size()
    - mempool_partitions_used()
    - mempool_decode_debug_info()

--- include/mempool.h
+++ include/mempool.h
@@ -5,6 +5,9 @@
 extern "C" {
 #endif
 
+#include "type.h"
+#include "common.h"
+
 /* ------------------------------------------------------------ */
 /* ---------------------------- Macros ------------------------ */
 /* ------------------------------------------------------------ */
@@ -15,6 +18,96 @@ extern "C" {
 #define MEMPOOL_API_VERSION_MINOR   1
 /** Revision version */
 #define MEMPOOL_API_VERSION_REVISION 0
+
+/* ------------------------------------------------------------ */
+/* -------------------------- Data types ---------------------- */
+/* ------------------------------------------------------------ */
+
+/** Return codes for API functions */
+typedef enum mempool_status_
+{
+    mempool_status_ok, /**< Success */
+    mempool_status_nok, /**< General error code */
+    mempool_status_size_err, /**< Pool size is not a power of two */
+    mempool_status_out_of_memory, /**< Out of memory */
+    mempool_status_nullptr /**< Unexpected NULL pointer */
+} mempool_status;
+
+/** Mempool instance holding all information */
+typedef struct mempool_instance_
+{
+    i8* base_addr; /**< Base address of the pool buffer */
+    u32 size; /**< Size of the pool buffer */
+} mempool_instance;
+
+/** Mempool debug info structure. May be used for testing purposes */
+typedef struct mempool_debug_info_
+{
+    bool is_first; /**< True if the partition does not have predecessor */
+    bool is_last; /**< True if the partition does not have successor */
+    bool room_occupied; /**< True if the partition is occupied */
+    u32 room_size; /**< Size of the partition */
+    u32 usable_size; /**< Size available for the user */
+} mempool_debug_info;
+
+/* ------------------------------------------------------------ */
+/* ----------------------- Public functions ------------------- */
+/* ------------------------------------------------------------ */
+
+/**
+ * Initialize mempool instance.
+ *
+ * The function has to be invoked before any other API functions. It creates a single memory partition that occupies all
+ * available space based on parameters inside 'pool' variable. Mempool module assumes that memory buffer was allocated
+ * prior to calling this API function and it will be freed outside this module. Memory buffer has to follow below rules:
+ *  1. Its size must be a power of two
+ *  2. It has to be large enough to contain partition header + 1 extra byte - use mempool_calc_hdr_size() to calculate
+ *     header length
+ *
+ * @param pool Pointer to a struct containing pool properties. The struct has to be initialized with valid values.
+ * @return Status of the operation:
+ *         - mempool_status_nullptr in case NULL was passed instead of a valid pointer
+ *         - mempool_status_size_err in case size of the memory buffer is not a power of two
+ *         - mempool_status_out_of_memory when the buffer is to small to allocate first partition
+ *         - mempool_status_nok in case of general failure that cannot be handled directly in the function
+ *         - mempool_status_ok on success
+ */
+mempool_status mempool_init(mempool_instance* pool);
+
+/**
+ * Calculate how many bytes are needed to store partition's metadata.
+ *
+ * This chunk of memory is excluded from general usage and it is hidden from the user (no need to manually move N bytes
+ * forward to get usable memory pointer).
+ *
+ * @return The number of bytes.
+ */
+u32 mempool_calc_hdr_size();
+
+/**
+ * Calculate how many partitions are available.
+ *
+ * The function counts both occupied and not occupied ones. The pool has to be initialized at the time this function is
+ * called.
+ *
+ * @param pool Pointer to a pool instance.
+ * @return The number of partitions used or zero when NULL was passed.
+ */
+u32 mempool_partitions_used(const mempool_instance* pool);
+
+/**
+ * Decode pool's debug data.
+ *
+ * The function may be called in a testing environment. None of the API functions use it directly. It fills the
+ * 'dbg_info' vector with 'mempool_debug_info' rows. Each row contains information about single partition created.
+ * The vector has to be large enough to accommodate all information. Its size can be calculated using
+ * mempool_partitions_used() API call. The function returns 0 if either 'pool' or 'dbg_info' pointers is NULL.
+ *
+ * @param pool Pointer to a pool instance.
+ * @param dbg_info Pointer to a debug vector.
+ * @return The number of rows written.
+ */
+u32 mempool_decode_debug_info(const mempool_instance* pool, mempool_debug_info* dbg_info);
 
 #ifdef __cplusplus
 }
--- unit_test/TestMempool.cpp
+++ unit_test/TestMempool.cpp
@@ -7,15 +7,99 @@
 
 TEST_GROUP(Mempool)
 {
-    void setup() override {}
-    void teardown() override {}
+    /* Buffer used for general testing */
+    static const u32 BUFFER_1K_SIZE = 1024;
+    i8* buffer1K = nullptr; /* Allocation and freeing handled in setup() and teardown() functions respectively. */
+
+    void setup() override
+    {
+        buffer1K = new i8[BUFFER_1K_SIZE];
+    }
+
+    void teardown() override
+    {
+        delete[] buffer1K;
+    }
+
+    auto initMempoolWith1KBuffer() const
+    {
+        mempool_instance inst;
+        inst.base_addr = buffer1K;
+        inst.size = BUFFER_1K_SIZE;
+        CHECK_EQUAL(mempool_status_ok, mempool_init(&inst));
+        return inst;
+    }
 };
 
 /* ------------------------------------------------------------ */
 /* ------------------------ Test cases ------------------------ */
 /* ------------------------------------------------------------ */
 
-TEST(Mempool, ToBeDeleted)
+TEST(Mempool, mempool_init__NullCases)
 {
-    CHECK_TRUE(true);
+    CHECK_EQUAL(mempool_status_nullptr, mempool_init(nullptr));
+
+    mempool_instance pool;
+    pool.base_addr = nullptr;
+    pool.size = BUFFER_1K_SIZE;
+    CHECK_EQUAL(mempool_status_nullptr, mempool_init(&pool));
+}
+
+TEST(Mempool, mempool_init__SizeNotPowerOf2__ErrorGenerated)
+{
+    const u32 invalidSizeTbl[] = {
+        0, /* Zero-case should also be handled */
+        3, 10, 127, 257, 1025, 2000, 6718291, UINT32_MAX
+    };
+
+    mempool_instance pool;
+    pool.base_addr = buffer1K;
+    for (const auto& size: invalidSizeTbl) {
+        pool.size = size; /* Set new size and call init function */
+        CHECK_EQUAL(mempool_status_size_err, mempool_init(&pool));
+    }
+}
+
+TEST(Mempool, mempool_init__BufferSizeTooSmall__ErrorReturned)
+{
+    mempool_instance pool;
+    pool.base_addr = buffer1K;
+    pool.size = mempool_calc_hdr_size();
+    CHECK_EQUAL(mempool_status_out_of_memory, mempool_init(&pool));
+}
+
+TEST(Mempool, mempool_init__ValidParams__Success)
+{
+    auto pool = initMempoolWith1KBuffer();
+    auto numOfRooms = mempool_partitions_used(&pool);
+    CHECK_EQUAL(1, numOfRooms);
+
+    mempool_debug_info dbgInfo[numOfRooms];
+    CHECK_EQUAL(1, mempool_decode_debug_info(&pool, dbgInfo));
+
+    mempool_debug_info* first_room_dbg = &dbgInfo[0];
+    CHECK_TRUE(first_room_dbg->is_first);
+    CHECK_TRUE(first_room_dbg->is_last);
+    CHECK_FALSE(first_room_dbg->room_occupied);
+    CHECK_EQUAL(BUFFER_1K_SIZE, first_room_dbg->room_size);
+    CHECK_EQUAL(BUFFER_1K_SIZE - mempool_calc_hdr_size(), first_room_dbg->usable_size);
+}
+
+TEST(Mempool, mempool_calc_hdr_size__EqualsTo32)
+{
+    auto hdrSize = mempool_calc_hdr_size();
+    CHECK_EQUAL(32, hdrSize);
+}
+
+TEST(Mempool, mempool_partitions_used__NullPassed__ZeroReturned)
+{
+    CHECK_EQUAL(0, mempool_partitions_used(nullptr));
+}
+
+TEST(Mempool, mempool_decode_debug_info__InvalidPointerPassed__ZeroReturnedWhen)
+{
+    auto pool = initMempoolWith1KBuffer();
+    mempool_debug_info dbgInfo;
+    CHECK_EQUAL(0, mempool_decode_debug_info(nullptr, &dbgInfo));
+    CHECK_EQUAL(0, mempool_decode_debug_info(&pool, nullptr));
 }
